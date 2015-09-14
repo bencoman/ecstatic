9ea5a75c4c17eaa5bbbcf8491f59cda5c0954d31
Ben Coman <btc@openInWorld.com>
2015-09-14 22:45:46 +0800

    Cache pharo download under _ecstatic/__cache

--- _ecstatic/install.sh
+++ _ecstatic/install.sh
@@ -1,3 +1,10 @@
-wget --quiet -O - get.pharo.org/40+vm | bash
+# Download Pharo infrastructure VM+Image under __cache folder
+# located at the same place as this script.
+
+ECSTATIC_DIR=`dirname $0`
+mkdir -p $ECSTATIC_DIR/__cache
+cd $ECSTATIC_DIR/__cache
+
+wget -O - get.pharo.org/40+vm | bash
 REPO=http://smalltalkhub.com/mc/StephaneDucasse/Ecstatic/main
 ./pharo Pharo.image config $REPO ConfigurationOfEcstatic --install=0.5 --group='ALL'
